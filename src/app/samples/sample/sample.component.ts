f3517283d944c12fe9f287896c087d14e5ce7e25
Aurélien Hugues <aurelien.hugues.59@gmail.com>
2017-12-24 21:18:00 +0100

    Cleanup application file tree

--- src/app/samples/sample/sample.component.ts
+++ src/app/samples/sample/sample.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sample',
   templateUrl: './sample.component.html',
-  styleUrls: ['./sample.component.css']
 })
 export class SampleComponent implements OnInit {
 
--- src/app/samples/samples.component.ts
+++ src/app/samples/samples.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-samples',
   templateUrl: './samples.component.html',
-  styleUrls: ['./samples.component.css']
 })
 export class SamplesComponent implements OnInit {
 
