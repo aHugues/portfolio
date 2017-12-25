75e4fa622def044f0e4addc052c8675fa6c47161
Aurélien Hugues <aurelien.hugues.59@gmail.com>
2017-12-25 17:44:41 +0100

    Add illustrations to samples page

--- src/app/samples/samples-list/samples.ts
+++ src/app/samples/samples-list/samples.ts
@@ -5,6 +5,7 @@ porygonSample.title = "Porygon";
 porygonSample.description = `
   Movies and series sorting and inventoring application with commands support
 `;
+porygonSample.imageLink = '/assets/porygon.png';
 
 
 let portfolioSample = new Sample();
@@ -12,6 +13,7 @@ portfolioSample.title = "Portfolio";
 portfolioSample.description = `
   This website (personal developer portfolio)
 `;
+portfolioSample.imageLink = '/assets/portfolio.png';
 
 
 export let samplesList = [porygonSample, portfolioSample];
